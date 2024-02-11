c1d2977f8fa32cb703cd77a7ce0e15c71efb18d6
lenka <lena.malicheva@mail.ru>
2024-02-11 23:04:40 +0400

    diplom add test

--- src/test/java/tests/PoliticTest.java
+++ src/test/java/tests/PoliticTest.java
@@ -6,8 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.io.InputStreamReader;
 
+import com.codeborne.pdftest.PDF;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -17,13 +18,12 @@ public class PoliticTest extends TestBase {
     void downloadFileTest() throws Exception {
         open("/company/");
         File downloaded =
-                $("p.a[href*='https://sibintek.ru/upload/iblock/5bf/%D0%9F%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0_%D0%B2_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D0%B8_%D0%BA%D0%B0%D1%87%D0%B5%D1%81%D1%82%D0%B2%D0%B0_1.pdf']")
+                $("p a[href*='/upload/']")
                         .download();
-
         try (InputStream is = new FileInputStream(downloaded)) {
-            byte[] data = is.readAllBytes();
-            String dataAsString = new String(data, StandardCharsets.UTF_8);
-            Assertions.assertTrue(dataAsString.contains("Политика в области качества"));
+            PDF pdf = new PDF(downloaded);
+            String pdfAuthor = pdf.author;
+            Assertions.assertEquals("mvkorshunova2", pdfAuthor);
         }
     }
 }
